c003a672099504808038f62a23644e9d9bd6e4c4
Jocadbz <madureirajoaquim95@gmail.com>
2023-01-08 21:26:18 -0300

    Add color modules

--- lazyfetch.v
+++ lazyfetch.v
@@ -1,11 +1,9 @@
 // Code Written by Jocadbz
 // MIT License
-// ansicolors module written by Abidin Durdu (betelgeuse-7) - Literally no license provided
 
 module main
 
 import functions
-import betelgeuse_7.ansicolors as colors
 
 fn main() {
 	// Change this array.
@@ -17,11 +15,11 @@ fn main() {
 	functions.shell(),
 	functions.desktop(),
 	functions.uptime(),
-	functions.term()
+	functions.term(),
 	]
 	// End of the array.
 	for func in commands {
-		println(colors.bold(func))
+		println(functions.bold(func))
 	}
 }
 
